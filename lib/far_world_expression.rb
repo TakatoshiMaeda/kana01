b46660c6de29ebdbe1d41a426840c314d329f018
Takatoshi Maeda <me@tmd.tw>
2015-01-17 14:03:14 +0900

    :bento:

--- lib/far_world_expression.rb
+++ lib/far_world_expression.rb
@@ -1,7 +1,83 @@
 # データ構造をどう持つか
+# 4*5+6&7|8
 class FarWorldExpression
   def initialize(expression)
-    left, operator, right = expression.scan(/([0-9]+)([\|\&\+\*])?(.*)?$/)
+    #left, operator, right = expression.scan(/([0-9]+)([\|\&\+\*])?(.*)?$/)
+    @expression = [
+      [
+        4, '*', [
+          5, '+', [
+            6, '&', [
+              7, '|', 8
+            ]
+          ],
+        ],
+      ],
+    ]
+
+    result = []
+
+    # or
+    posisions = select_positions('|')
+    posisions.each do |idx|
+      exp = @expression[idx]
+      result[idx] << "(#{exp.join})"
+      unless result[idx - 1].nil?
+
+      end
+    end
+  end
+
+  def parse
+    [
+      [
+        4, '*', [
+          5, '+', [
+            6, '&', [
+              7, '|', 8
+            ]
+          ],
+        ],
+      ],
+    ]
+  end
+
+  def select_posisions(operator)
+    positions = []
+    @expression.each_with_index do |exp, idx|
+      if exp[2] == operator
+        posisions << idx
+      end
+    end
+    positions
+  end
+
+  def and_posisions
+
+  end
+
+  def first_posision
+
+    @expression.each_with_index do |exp, idx|
+      if exp[2] == '&'
+        position = idx
+      end
+    end
+    return position unless position.nil?
+
+    @expression.each_with_index do |exp, idx|
+      if exp[2] == '+'
+        position = idx
+      end
+    end
+    return position unless position.nil?
+
+    @expression.each_with_index do |exp, idx|
+      if exp[2] == '*'
+        position = idx
+      end
+    end
+    return position unless position.nil?
   end
 
   def evaluate
